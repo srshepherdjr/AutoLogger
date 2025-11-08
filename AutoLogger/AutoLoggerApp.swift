2d91225a30922ad0bd0b0fdce793c290cced9c68
Steven Shepherd <srshepherdjr@gmail.com>
2025-11-08 08:39:25 -0500

    Initial Commit

---
+++ AutoLogger/AutoLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLoggerApp.swift
+//  AutoLogger
+//
+//  Created by Steven Shepherd on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

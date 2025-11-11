6231b0a4c209f29814be7c90fb94bd73d61c4963
Steven Shepherd <srshepherdjr@gmail.com>
2025-11-10 20:37:12 -0500

    updates to gas editor

--- AutoLogger/GasView.swift
+++ AutoLogger/GasView.swift
@@ -15,89 +15,128 @@ struct GasView: View {
     @State var gas: AutoCarGas
     @State private var myNumber: Int? = 0
     
+    @FocusState private var focusField: Field?
+    
+    enum Field {
+        case gasDate, startMiles, endMiles, netMiles, gallons, pricePerGallon
+    }
+
     let car: AutoCar
     
     var body: some View {
         Text("\(car.modelYear) " + car.carMake + " " + car.carModel)
         Form {
-            HStack {
-                Text("ID")
-                Spacer()
-                Text("\(gas.id)")
-            }
-            HStack {
-                Text("Date")
-                Spacer()
-                if isEditing {
-                    TextField("Pick a date.", text: $gas.cargasdate)
-                } else {
-                    Text(gas.cargasdate)
+            Section(header: Text("Fill up details")) {
+                HStack {
+                    Text("ID")
+                    Spacer()
+                    Text("\(gas.id)")
                 }
-            }
-            HStack {
-                Text("Start miles")
-                Spacer()
-                if isEditing {
-                    TextField("Start miles", value: $gas.carmilesstart, formatter: Self.numberFormatter)
-                        .keyboardType(.numberPad) // Suggests a numeric keyboard
-                        .border(Color.gray)
-                } else {
-                    Text("\(gas.carmilesstart)")
+                HStack {
+                    Text("Date")
+                    Spacer()
+                    if isEditing {
+                        TextField("Pick a date.", text: $gas.cargasdate)
+                            .focused($focusField, equals: .gasDate)
+                            .submitLabel(.next)
+                            .onSubmit {
+                                focusField = .startMiles
+                            }
+                    } else {
+                        Text(gas.cargasdate)
+                    }
                 }
-            }
-            HStack {
-                Text("Start end")
-                Spacer()
-                if isEditing {
-                    TextField("End miles", value: $gas.carmilesend, formatter: Self.numberFormatter)
-                        .keyboardType(.numberPad) // Suggests a numeric keyboard
-                        .border(Color.gray)
-                } else {
-                    Text("\(gas.carmilesend)")
+                HStack {
+                    Text("Start miles")
+                    Spacer()
+                    if isEditing {
+                        TextField("Start miles", value: $gas.carmilesstart, formatter: Self.numberFormatter)
+//                            .keyboardType(.numberPad) // Suggests a numeric keyboard
+                            .border(Color.gray)
+                            .focused($focusField, equals: .startMiles)
+                            .submitLabel(.next)
+                            .onSubmit {
+                                focusField = .endMiles
+                            }
+                    } else {
+                        Text("\(gas.carmilesstart)")
+                    }
                 }
-            }
-            HStack {
-                Text("Start net")
-                Spacer()
-                if isEditing {
-                    TextField("Net miles", value: $gas.carmilesnet, formatter: Self.numberFormatter)
-                        .keyboardType(.numberPad) // Suggests a numeric keyboard
-                        .border(Color.gray)
-                } else {
-                    Text("\(gas.carmilesnet)")
+                HStack {
+                    Text("Start end")
+                    Spacer()
+                    if isEditing {
+                        TextField("End miles", value: $gas.carmilesend, formatter: Self.numberFormatter)
+//                            .keyboardType(.numberPad) // Suggests a numeric keyboard
+                            .border(Color.gray)
+                            .focused($focusField, equals: .endMiles)
+                            .submitLabel(.next)
+                            .onSubmit {
+                                focusField = .netMiles
+                            }
+                    } else {
+                        Text("\(gas.carmilesend)")
+                    }
                 }
-            }
-            HStack {
-                Text("Gallons")
-                Spacer()
-                if isEditing {
-                    TextField("Gallons", value: $gas.cargals, formatter: Self.numberFormatter)
-                        .keyboardType(.numberPad) // Suggests a numeric keyboard
-                        .border(Color.gray)
-                } else {
-                    Text(gas.cargals.formatted())
+                HStack {
+                    Text("Start net")
+                    Spacer()
+                    if isEditing {
+                        TextField("Net miles", value: $gas.carmilesnet, formatter: Self.numberFormatter)
+//                            .keyboardType(.numberPad) // Suggests a numeric keyboard
+                            .border(Color.gray)
+                            .focused($focusField, equals: .netMiles)
+                            .submitLabel(.next)
+                            .onSubmit {
+                                focusField = .gallons
+                            }
+                    } else {
+                        Text("\(gas.carmilesnet)")
+                    }
                 }
-            }
-            HStack {
-                Text("Price / gal")
-                Spacer()
-                if isEditing {
-                    TextField("Price / gal", value: $gas.cargasprice, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.numberPad) // Suggests a numeric keyboard
-                        .border(Color.gray)
-                } else {
-                    Text(gas.cargasprice?.formatted(.currency(code: "USD")) ?? "0.00")
+                HStack {
+                    Text("Gallons")
+                    Spacer()
+                    if isEditing {
+                        TextField("Gallons", value: $gas.cargals, formatter: Self.numberFormatter)
+//                            .keyboardType(.numberPad) // Suggests a numeric keyboard
+                            .border(Color.gray)
+                            .focused($focusField, equals: .gallons)
+                            .submitLabel(.next)
+                            .onSubmit {
+                                focusField = .pricePerGallon
+                            }
+                    } else {
+                        Text(gas.cargals.formatted())
+                    }
                 }
-            }
-            Spacer()
-            HStack {
-                Spacer()
-                Button("Save") {
-                    dismiss()
+                HStack {
+                    Text("Price / gal")
+                    Spacer()
+                    if isEditing {
+                        TextField("Price / gal", value: $gas.cargasprice, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//                            .keyboardType(.numberPad) // Suggests a numeric keyboard
+                            .border(Color.gray)
+//                            .focused($focusField, equals: .pricePerGallon)
+                            .submitLabel(.done)
+                            .onSubmit {
+                                focusField = nil
+                                SubmitForm()
+                            }
+                    } else {
+                        Text(gas.cargasprice?.formatted(.currency(code: "USD")) ?? "0.00")
+                    }
                 }
-                Button("Back") {
-                    dismiss()
-                }
+//                Spacer()
+//                HStack {
+//                    Spacer()
+//                    Button("Save") {
+//                        dismiss()
+//                    }
+//                    Button("Back") {
+//                        dismiss()
+//                    }
+//                }
             }
         }
     }
@@ -109,6 +148,10 @@ struct GasView: View {
         formatter.maximumFractionDigits = 0 // For integers
         return formatter
     }()
+    
+    private func SubmitForm() {
+        print("Form submitted")
+    }
 }
 
 #Preview {
